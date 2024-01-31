4b4a939233eda9e8d5716b018a68fa475812c7a7
willian pereira <willianpereira0912@gmail.com>
2024-01-30 23:09:13 -0300

    feat: add initial barbershop-item component

---
+++ src/app/(home)/_components/barbershop-item.tsx
@@ -0,0 +1,52 @@
+import { Badge } from "@/app/_components/ui/badge";
+import { Button } from "@/app/_components/ui/button";
+import { Card, CardContent } from "@/app/_components/ui/card";
+import { BarberShop } from "@prisma/client";
+import { StarIcon } from "lucide-react";
+import Image from "next/image";
+
+type BarberShopProps = {
+  barbershop: BarberShop;
+};
+
+const BarberShopItem = ({ barbershop }: BarberShopProps) => {
+  return (
+    <Card className="min-w-[167px] max-w-[167px] rounded-2xl">
+      <CardContent className="px-1 pb-0">
+        <div className="px-1 relative w-full h-[159px]">
+          <div className="absolute left-2 top-2 z-50"> 
+            <Badge variant="secondary" className="flex items-center gap-2 opacity-90">
+              <StarIcon size={12} className="fill-primary text-primary"/>
+              <span className="text-xs">5,0</span>
+            </Badge>
+          </div>
+          <Image
+            src={barbershop.imageUrl}
+            width={0}
+            height={0}
+            alt={barbershop.name}
+            sizes="100vw"
+            fill
+            style={{
+              objectFit: "cover",
+            }}
+            className="h-[159px]  rounded-2xl"
+          />
+        </div>
+        <div className="px-2 pb-3">
+          <h2 className="font-bold mt-2 overflow-hidden text-ellipsis text-nowrap">
+            {barbershop.name}
+          </h2>
+          <p className="text-sm text-gray-400 overflow-hidden text-ellipsis text-nowrap">
+            {barbershop.address}
+          </p>
+          <Button variant="secondary" className="w-full mt-3">
+            Reservar
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default BarberShopItem;
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,8 +3,13 @@ import Header from "../_components/header";
 import { format } from "date-fns";
 import Search from "./_components/Search";
 import BookingItem from "../_components/booking-item";
+import { db } from "../_lib/prisma";
+import BarberShopItem from "./_components/barbershop-item";
 
-export default function Home() {
+export default async function Home() {
+
+  const barberShop = await db.barberShop.findMany()
+
   return (
     <div>
       <Header />
@@ -23,6 +28,13 @@ export default function Home() {
       <div className="px-5 mt-6">
         <h2 className="text-xs uppercase text-gray-400 font-bold mb-3">Agendamentos</h2>
         <BookingItem/>
+      </div>
+
+      <div className="mt-6 px-4">
+       <h2 className="text-xs uppercase text-gray-400 font-bold mb-3">Recomendados</h2>
+        <div className="grid grid-cols-2 gap-4">
+         {barberShop.map(item => <BarberShopItem barbershop={item} key={item.id}/>)}
+        </div>
       </div>
     </div>
   );
