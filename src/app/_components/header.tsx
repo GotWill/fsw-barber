0400d9d8d6e3c6c92a0158139ae6297473d6fa77
willian pereira <willianpereira0912@gmail.com>
2024-02-12 15:00:05 -0300

    feat: add style header deks

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -2,12 +2,16 @@
 import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
-import { MenuIcon } from "lucide-react";
+import { CalendarDays, MenuIcon, UserCircle } from "lucide-react";
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import SideMenu from "./side-menu";
 import Link from "next/link";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Avatar, AvatarImage } from "./ui/avatar";
 
 const Header = () => {
+  const { data, status } = useSession();
+
   return (
     <header>
       <Card>
@@ -15,16 +19,43 @@ const Header = () => {
           <Link href="/">
             <Image src="/logo.png" alt="logo" width={120} height={22} />
           </Link>
-          <Sheet>
-            <SheetTrigger asChild>
-              <Button variant="outline" size="icon">
-                <MenuIcon size={18} />
-              </Button>
-            </SheetTrigger>
-            <SheetContent className="p-0">
-              <SideMenu />
-            </SheetContent>
-          </Sheet>
+          <div className="md-desk:hidden">
+            <Sheet>
+              <SheetTrigger asChild>
+                <Button variant="outline" size="icon">
+                  <MenuIcon size={18} />
+                </Button>
+              </SheetTrigger>
+              <SheetContent className="p-0">
+                <SideMenu />
+              </SheetContent>
+            </Sheet>
+          </div>
+          <div className="md-mobol:hidden flex gap-4 items-center">
+            <div className="flex gap-2">
+              <CalendarDays />
+              <Link href="/bookings">
+                <span className="font-bold text-sm">Agendamentos</span>
+              </Link>
+            </div>
+            <div className="flex gap-2">
+              {status === "unauthenticated" ? (
+                <Button className="flex gap-2" onClick={() => signIn("google")}>
+                  <UserCircle />
+                  <span>Perfil</span>
+                </Button>
+              ) : (
+                <div className="flex items-center gap-2">
+                  <Avatar>
+                    <AvatarImage src={`${data?.user?.image}`} />
+                  </Avatar>
+                  <span className="font-bold text-base">
+                    {data?.user?.name}
+                  </span>
+                </div>
+              )}
+            </div>
+          </div>
         </CardContent>
       </Card>
     </header>
